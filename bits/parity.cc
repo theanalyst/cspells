b74fcde6b2ac50edc87b8f8b2d05999475640a0a
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2016-06-22 22:46:26 +0200

    bits: parity v1

---
+++ bits/parity.cc
@@ -0,0 +1,38 @@
+#include <bitset>
+#include <iostream>
+#include <cstdint>
+
+#define check_bit(n,pos) (n & (1<<pos))
+
+using namespace std;
+bool odd_parity(unsigned long n){
+    std::bitset<64> bits (n);
+    return bits.count()%2 != 0;
+}
+
+bool odd_parity2(unsigned long n){
+    int count=0;
+    for(int i=0;i<sizeof(n);i++){
+	if(n & (1 << i)){
+	    count++;
+	}
+	    
+	
+    }
+	
+    return count%2 != 0;
+}
+
+int main(int argc, char *argv[])
+{
+    unsigned long n1(2),n2(10),n3(7);
+    cout << odd_parity(n1) << endl;
+    cout << odd_parity2(n1) << endl;
+    
+    cout << odd_parity(n2) << endl;
+    cout << odd_parity2(n2) << endl;
+
+    cout << odd_parity(n3) << endl;
+    cout << odd_parity2(n3) << endl;
+    return 0;
+}
