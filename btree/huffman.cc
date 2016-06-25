e0bc1dc72d065eb34eef7cced8f8e5bb565ce856
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2016-06-25 16:35:51 +0200

    btree: huffman moar constructors

--- btree/huffman.cc
+++ btree/huffman.cc
@@ -1,25 +1,22 @@
 #include <iostream>
 #include <string>
-
+// given a huffman tree decode the string;
 using namespace std;
-
 struct node
 {
     int freq;
     char data;
-    node * left;
-    node * right;
-
-
+    node* left;
+    node* right;
     node(int f, char c) : freq(f), data(c), left(nullptr), right(nullptr){};
-    node(int f, node *left, node *right) : freq(f), data('\0'), left(left), right(right) {};
-    node(int f, char c, node *left, node * right) : freq(f), data(c), left(left), right(right) {};
+    node(int f, node* left, node* right) : freq(f), data('\0'), left(left), right(right) {};
+    node(int f, char c, node* left, node* right) : freq(f), data(c), left(left), right(right) {};
 };
 
 string decode_huff(node * root,string s)
 {
     string out;
-    node * iter = root;
+    node* iter = root;
     for (auto const &i:s){
 	iter = i == '0' ? iter->left : iter->right;
 	if (iter->data){
@@ -39,6 +36,6 @@ int main(int argc, char *argv[])
     node left = node(2, &B, &C);
     node top = node(5, &left, &A);
 
-    cout << decode_huff(&top,"1001011");
+    std::cout << decode_huff(&top,"1001011");
     return 0;
 }
