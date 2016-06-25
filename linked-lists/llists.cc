94427529b3c53189fe25cebdafcd0544173a04b2
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2016-06-25 16:35:04 +0200

    merge ll code
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>

--- linked-lists/llists.cc
+++ linked-lists/llists.cc
@@ -77,6 +77,25 @@ void Print(Node *head){
     cout << endl;
 }
 
+
+Node * mergeLists(Node *head1, Node *head2){
+    Node *newhead = head1->data > head2->data ? head2 : head1;
+    Node *iter = newhead;
+    Node *iter2 = head1->data > head2->data ? head1 : head2;
+
+    while (iter || iter2){
+	if (!iter->next){
+	    iter->next = iter2;
+	    break;
+	} else if (iter->next->data > iter2->data){
+	    std::swap(iter->next,iter2);
+	}
+
+	iter = iter->next;
+    }
+    return newhead;
+}
+
 int main(int argc, char *argv[])
 {
     Node * list1;
@@ -90,5 +109,29 @@ int main(int argc, char *argv[])
     cout << "Deleting at position 2" << endl;
     Delete(list1,2);
     Print(list1);
+
+    Node *list2;
+    list2 = new Node;
+    list2 = insert (NULL, 8);
+    list2 = insert_head(list2, 6);
+    list2 = insert_head(list2, 4);
+    list2 = insert_head(list2, 2);
+    // list2 = insert(list2, 6);
+    // list2 = insert(list2, 8);
+
+    Node *list3;
+    list3 = new Node;
+    list3 = insert (NULL, 5);
+    list3 = insert_head(list3, 3);
+    list3 = insert_head(list3, 1);
+
+    // Node *list3 = new Node;
+    // list3 = insert(NULL,1);
+    // list3 = insert(list3,3);
+    // list3 = insert(list3,5);
+
+    Node *listm  = new Node;
+    listm = mergeLists(list2,list3);
+    Print(listm);
     return 0;
 }
