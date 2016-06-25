1e0c65b42d25d698d05c77f1fcf192f1895b736a
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2016-06-25 16:36:20 +0200

    bits: yet another parity check

--- bits/parity.cc
+++ bits/parity.cc
@@ -1,6 +1,7 @@
 #include <bitset>
 #include <iostream>
 #include <cstdint>
+#include <cmath>
 
 #define check_bit(n,pos) (n & (1<<pos))
 
@@ -23,16 +24,26 @@ bool odd_parity2(unsigned long n){
     return count%2 != 0;
 }
 
+bool odd_parity3(unsigned long n){
+    int parity;
+    while(n){
+	parity ^= 1;
+	n=n & (n - 1);
+    }
+    return parity != 0;
+}
+
 int main(int argc, char *argv[])
 {
     unsigned long n1(2),n2(10),n3(7);
     cout << odd_parity(n1) << endl;
     cout << odd_parity2(n1) << endl;
-    
+    cout << odd_parity3(n1) << endl;
     cout << odd_parity(n2) << endl;
     cout << odd_parity2(n2) << endl;
-
+    cout << odd_parity3(n2) << endl;
     cout << odd_parity(n3) << endl;
     cout << odd_parity2(n3) << endl;
+    cout << odd_parity3(n3) << endl;
     return 0;
 }
