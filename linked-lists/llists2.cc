dd8f9b81657632e9e731cf252fd2d65deb84ca03
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2016-06-26 12:15:44 +0200

    evenodd

--- linked-lists/llists2.cc
+++ linked-lists/llists2.cc
@@ -31,11 +31,40 @@ void insert_head(std::shared_ptr <node<T> > &n, T d){
     }
 }
 
+template <typename T>
+void evenodd(std::shared_ptr <node <T>> &n){
+    auto iter = n;
+    if (n->next == nullptr)
+	return;
+    auto odd_head = n->next;
+    auto odd_iter = odd_head;
+    while (iter->next->next != nullptr){
+	iter->next = iter->next->next;
+	if (odd_iter->next->next == nullptr){
+	    iter = iter->next;
+	    odd_iter->next = nullptr;
+	    break;
+	}
+	odd_iter->next = odd_iter->next->next;
+	iter = iter->next;
+	odd_iter = odd_iter->next;
+    }
+    cout << endl;
+    iter->next = odd_head;
+}
+
 int main(int argc, char *argv[])
 {
     std::shared_ptr<node <int>> N;
     insert_head(N,2);
     insert_head(N,1);
+    insert_head(N,0);
+    insert_head(N,10);
+    insert_head(N,20);
+    insert_head(N,30);
+    insert_head(N,42);
+    printlist(N);
+    evenodd(N);
     printlist(N);
     return 0;
 }
